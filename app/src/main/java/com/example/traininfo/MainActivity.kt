c597e65a34ea4686a446a39154934b7accd55c4e
Achintya Sharma <sharmaachintya08@gmail.com>
2023-03-19 23:38:04 +0530

    created splash screen

--- app/src/main/java/com/example/traininfo/MainActivity.kt
+++ app/src/main/java/com/example/traininfo/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.example.traininfo
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.WindowManager
+import com.example.traininfo.Authentication.Login
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this@MainActivity, Login::class.java)
+            startActivity(intent)
+            finish()
+        }, 2000)
     }
 }
