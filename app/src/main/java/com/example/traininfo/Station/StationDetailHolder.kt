866181504d9d325945345ae989da4f80079c4f84
Achintya Sharma <sharmaachintya08@gmail.com>
2023-03-21 03:22:27 +0530

    created space for fragments

---
+++ app/src/main/java/com/example/traininfo/Station/StationDetailHolder.kt
@@ -0,0 +1,12 @@
+package com.example.traininfo.Station
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.traininfo.R
+
+class StationDetailHolder : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_station_detail_holder)
+    }
+}
