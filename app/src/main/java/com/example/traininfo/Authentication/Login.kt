0f8aa273d92838c8ab23440c688429b43733a796
Achintya Sharma <sharmaachintya08@gmail.com>
2023-03-24 03:04:37 +0530

    added some logic to change activity_login.xml to activity_signup.xml

--- app/src/main/java/com/example/traininfo/Authentication/Login.kt
+++ app/src/main/java/com/example/traininfo/Authentication/Login.kt
@@ -1,12 +1,45 @@
 package com.example.traininfo.Authentication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.TextView
+import androidx.appcompat.widget.AppCompatImageButton
 import com.example.traininfo.R
+import com.google.android.material.button.MaterialButton
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textview.MaterialTextView
 
 class Login : AppCompatActivity() {
+    private lateinit var googleAuthenticationButton : AppCompatImageButton
+    private lateinit var emailForAuthentication : TextInputEditText
+    private lateinit var passwordForAuthentication : TextInputEditText
+    private lateinit var loginButton : MaterialButton
+    private lateinit var forgotPasswordButton : MaterialTextView
+    private lateinit var signupButton : MaterialTextView
+
+    private lateinit var openActivity : Intent
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        initViews()
+        openSignupActivity()
+    }
+    private fun initViews(){
+        googleAuthenticationButton = findViewById(R.id.googleauthenticationbutton)
+        emailForAuthentication = findViewById(R.id.emailedittext)
+        passwordForAuthentication = findViewById(R.id.passwordedittext)
+        loginButton  = findViewById(R.id.loginbutton)
+        forgotPasswordButton = findViewById(R.id.forgotpasswordtextview)
+        signupButton = findViewById(R.id.signuptextview)
+    }
+    private fun openSignupActivity(){
+        signupButton.setOnClickListener(View.OnClickListener {view ->
+            openActivity = Intent(this@Login,Signup::class.java)
+            startActivity(openActivity)
+        })
     }
 }
