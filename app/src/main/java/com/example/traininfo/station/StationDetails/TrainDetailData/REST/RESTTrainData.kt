c6b27d7ac338c4f14f2ce567f9852fb810c52e96
Achintya Sharma <sharmaachintya08@gmail.com>
2023-04-10 00:14:50 +0530

    removed okhttp statements

--- app/src/main/java/com/example/traininfo/station/StationDetails/TrainDetailData/REST/RESTTrainData.kt
+++ app/src/main/java/com/example/traininfo/station/StationDetails/TrainDetailData/REST/RESTTrainData.kt
@@ -2,9 +2,11 @@ package com.example.traininfo.station.StationDetails.TrainDetailData.REST
 
 import android.util.Log
 import kotlinx.coroutines.*
+import okhttp3.Callback
 import okhttp3.OkHttpClient
 import okhttp3.Request
 import okhttp3.Response
+import org.json.JSONObject
 import java.io.IOException
 import java.util.Date
 
@@ -23,20 +25,19 @@ class RESTTrainData {
         currentDate = date
     }
     fun getTrainData(){
-        GlobalScope.launch(newSingleThreadContext("thread1")) {
-            val networkRequest = doNetworkRequest()
-            Log.d(TAG, networkRequest.toString())
+        GlobalScope.launch {
+            doNetworkRequest()
         }
     }
-    suspend fun doNetworkRequest() : Response{
-        val client = OkHttpClient()
-        val request = Request.Builder()
-            .url("https://indian-railway-irctc.p.rapidapi.com/getTrainId?trainno=1205")
-            .get()
-            .addHeader("X-RapidAPI-Key", "2b52c313b2msh26a780c5132d231p13a985jsne54b2cb597e7")
-            .addHeader("X-RapidAPI-Host", "indian-railway-irctc.p.rapidapi.com")
-            .build()
-        val response = client.newCall(request).execute()
-        return response
+    suspend fun doNetworkRequest(){
+        try {
+
+        }catch(e : java.lang.Exception) {
+
+        }
+    }
+
+    private fun printJSON(response: Response) {
+
     }
 }
